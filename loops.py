8c1374b2416de5da1fced5ec0ae52ec7e319b867
Corey <aingc@users.noreply.github.com>
2021-02-23 13:21:00 -0800

    Quick fizzbuzz in loops.py

--- loops.py
+++ loops.py
@@ -28,3 +28,14 @@ count = 0
 while count <= 10:
   print(f'Count: {count}')
   count += 1
+
+for i in range(1, 101):
+  if i % 3 == 0 and i % 5 == 0:
+    print('fizzbuzz')
+
+  if i % 3 == 0:
+    print('fizz')
+  elif i % 5 == 0:
+    print('buzz')
+  else:
+    print(i)
