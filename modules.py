ce1f667b2d04566c7fe800d94cdecc6e95e2957f
Corey <aingc@users.noreply.github.com>
2021-02-22 23:58:17 -0800

    Custom module validator.py to include in modules.py

--- modules.py
+++ modules.py
@@ -12,12 +12,22 @@ from time import time
 # Pip module
 from camelcase import CamelCase
 
+# import custom module
+import validator
+from validator import validate_email
+
 # today = datetime.date.today()
 today = date.today()
 timestamp = time()
 
 c = CamelCase()
 print(c.hump('hello there world'))
+
+email = 'test@test.com'
+if validate_email(email):
+  print('Email is valid')
+else:
+  print('Email is invalid')
 
 print(today, timestamp)
 
