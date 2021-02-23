f868f3bd01eed576223b56296902f64159aebe56
Corey <aingc@users.noreply.github.com>
2021-02-22 23:03:39 -0800

    modules

--- modules.py
+++ modules.py
@@ -3,14 +3,23 @@ A module is a file containing a set of funcs to include in an application.
 There are core python modules, modules you can install using pip pkg manager
 (including Django) as well as custom modules
 '''
-# a core python module: datetime
+# Core modules
 import datetime
 from datetime import date
 import time
 from time import time
 
+# Pip module
+from camelcase import CamelCase
+
 # today = datetime.date.today()
 today = date.today()
 timestamp = time()
 
+c = CamelCase()
+print(c.hump('hello there world'))
+
 print(today, timestamp)
+
+# pip3 install, installs globally, like npm install -g
+# pip3 freeze, will show all modules installed, in this case, it's in a global scope
